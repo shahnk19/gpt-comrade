c6aead9bc881d089d2dabebc89716ff82cf5eb77
shahnk19 <shahnk19@gmail.com>
2023-04-10 14:24:36 +0800

    Get streaming response

--- pkg/client.go
+++ pkg/client.go
@@ -1,42 +1,58 @@
 package pkg
 
 import (
-	"os"
 	"context"
+	"errors"
 	"fmt"
+	"io"
+	"os"
 
 	 openai "github.com/sashabaranov/go-openai"
 )
 
 func FetchFix(command string, terminalStatus error, errorMessage string, cheeky bool) {	
 	client := openai.NewClient(os.Getenv("OPENAI_API_KEY"))
-	resp, err := client.CreateChatCompletion(
-		context.Background(),
-		openai.ChatCompletionRequest{
-			Model: openai.GPT3Dot5Turbo,
-			Messages: []openai.ChatCompletionMessage{
-				{
-					Role:    openai.ChatMessageRoleSystem,
-					Content: "You are a helpful assistant. You can find solution for any command line errors given to you.",
-				},
-				{
-					Role:    openai.ChatMessageRoleUser,
-					Content: "I tried to run this command in my terminal: '" + command +
-									 "' It is giving this error: " + errorMessage + "." +
-									 terminalStatusPrompt(terminalStatus) +
-									 "Can you help to find the problem and what can I do to fix it?" +
-									 cheekyPrompt(cheeky),
-				},
+
+	req := openai.ChatCompletionRequest{
+		Model: openai.GPT3Dot5Turbo,
+		Messages: []openai.ChatCompletionMessage{
+			{
+				Role:    openai.ChatMessageRoleSystem,
+				Content: "You are a helpful assistant. You can find solution for any command line errors given to you. You would give a step by step solution complete with code fixes or commands.",
+			},
+			{
+				Role:    openai.ChatMessageRoleUser,
+				Content: "I tried to run this command in my terminal: '" + command +
+								 "' It is giving this error: " + errorMessage + "." +
+								 terminalStatusPrompt(terminalStatus) +
+								 "Can you help to find the problem and what can I do to fix it?" +
+								 cheekyPrompt(cheeky),
 			},
 		},
-	)
+		Stream: true,
+	}
 
+	stream, err := client.CreateChatCompletionStream(context.Background(), req)
 	if err != nil {
 		fmt.Printf("Appologies comrade, there is an error: %v\n", err)
 		return
 	}
+	defer stream.Close()
 
-	fmt.Println(resp.Choices[0].Message.Content)
+	for {
+		response, err := stream.Recv()
+		if errors.Is(err, io.EOF) {
+			fmt.Println("\nStream finished")
+			return
+		}
+
+		if err != nil {
+			fmt.Printf("\nStream error: %v\n", err)
+			return
+		}
+
+		fmt.Printf(response.Choices[0].Delta.Content)
+	}
 }
 
 func terminalStatusPrompt(terminalStatus error) string {
